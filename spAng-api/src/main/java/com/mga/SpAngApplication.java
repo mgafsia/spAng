0d04ca40727b27615aac786d69330f1772fbfb02
mgafsia-fa <mgafsia@financeactive.com>
2017-03-29 14:59:38 +0200

    first commit(2)

---
+++ spAng-api/src/main/java/com/mga/SpAngApplication.java
@@ -0,0 +1,12 @@
+package com.mga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpAngApplication.class, args);
+	}
+}
