b181af0d8f067662584e5f9df50ec94c06ee6585
mgafsia-fa <mgafsia@financeactive.com>
2017-04-04 09:12:52 +0200

    spang : add hike module(2) - KO.

--- spAng-web/src/main/spAng-web/src/app/app.module.ts
+++ spAng-web/src/main/spAng-web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {HikeModule} from "../hike/hike.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HikeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
