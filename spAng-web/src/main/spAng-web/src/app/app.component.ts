777a01aa583ce5567dd586f13d105ea2aa772c55
mgafsia-fa <mgafsia@financeactive.com>
2017-04-06 14:48:12 +0200

    spang : Add hikeList in hike component.

--- spAng-web/src/main/spAng-web/src/app/app.component.ts
+++ spAng-web/src/main/spAng-web/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HikeService} from "../hike/hike.service";
 import {Hike} from "../hike/Hike";
+import {HikeListComponent} from "../hike/hike-list.compenent";
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,18 @@ import {Hike} from "../hike/Hike";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title : string = "-";
-  hikes  : Hike[];
+  title: string = "-";
+  hikes: Hike[];
+  hikeListComponent: HikeListComponent;
 
   constructor(private _hikeService: HikeService) {
   }
 
   ngOnInit() {
     this.hikes = this._hikeService.getHikes();
+    this.hikeListComponent = new HikeListComponent(this._hikeService);
     console.log(this.hikes);
+    console.log(this.hikeListComponent);
     //TODO(MGA) : check the video. ngFor in ?
   }
 }
--- spAng-web/src/main/spAng-web/src/hike/hike-list.compenent.ts
+++ spAng-web/src/main/spAng-web/src/hike/hike-list.compenent.ts
@@ -1,11 +1,19 @@
 import {Component} from "@angular/core";
 import {Hike} from "./Hike";
+import {HikeService} from "./hike.service";
 
 @Component({
   selector: 'mga-hike-list',
   templateUrl: 'hike-list.component.html'
 })
-
 export class HikeListComponent {
   hikes : Hike[];
+
+  constructor(private _hikeService: HikeService) {
+  }
+
+  ngOnInit() {
+    this.hikes = this._hikeService.getHikes();
+  }
 }
+
