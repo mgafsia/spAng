cfa5a0444fb4412fec85bbe9e390ad52c963a9e9
mgafsia <mohamed.gafsia@gmail.com>
2017-06-02 08:23:41 +0200

    spang-web : use Observable.from to create Rx object as with Rx.umd.js.

--- spAng-web/src/main/spAng-web/src/rx/myobservable.component.ts
+++ spAng-web/src/main/spAng-web/src/rx/myobservable.component.ts
@@ -1,22 +1,22 @@
-import {Component} from "@angular/core";
-import {Observable} from "rxjs/Observable";
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
-import {MyObservor} from "./myobserver";
+
 
 @Component({
-  selector: 'mga-obsval',
+  selector: 'app-obsval',
   templateUrl: 'myobservable.component.html'
 })
 export class MyObservableComponent {
-    _intStream: [number] = [1,2,3,4,5];
+    _intStream: [number] = [1, 2, 3, 4, 5];
     myObservable$: Observable<number>;
 
     createObservable() {
       this.myObservable$ =  Observable.create((observer: any) => {
-        for(let i of this._intStream) {
+        for ( const i of this._intStream ) {
           observer.next(i);
-          if(i > 3) {
-            observer.error("not allowed !!");
+          if ( i > 3 ) {
+            observer.error('not allowed !!');
           }
         }
         observer.complete();
@@ -26,6 +26,15 @@ export class MyObservableComponent {
         val => console.log(`- val is : ${val}`),
         err => console.error(`- err is : ${err}`),
         ()  => console.log(`end !`)
+      );
+    }
+
+    createObservable2() {
+      const observable$ = Observable.from(this._intStream);
+      const observor = observable$.subscribe(
+        (val) => console.log(val),
+        (err) => console.error(err),
+        () => console.log('complete !')
       );
     }
 }
